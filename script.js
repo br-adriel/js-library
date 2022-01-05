3620e564314da3ed53299362f6450cb017b6f85b
Adriel <adriel.fariasan@gmail.com>
2022-01-04 23:29:24 -0300

    Implementa formulario de novo livro

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ btnNovoLivro.addEventListener("click", () => {
 btnFecharModal.addEventListener("click", (e) => {
   fundoModal.style.display = "none";
 });
+
+const ocultarModal = () => (fundoModal.style.display = "none");
+
+// Inputs do formulario
+const inputTitulo = document.getElementById("form-titulo");
+const inputAutor = document.getElementById("form-autor");
+const inputPaginas = document.getElementById("form-paginas");
+const inputLido = document.getElementById("form-lido");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  enviarForm(e);
+});
+
+function enviarForm(e) {
+  e.preventDefault();
+  adicionarNaBiblioteca();
+  ocultarModal();
+  form.reset();
+}
+
+function adicionarNaBiblioteca() {
+  if (
+    // verifica se os campos estao preenchidos
+    inputTitulo.value !== "" &&
+    inputAutor.value !== "" &&
+    inputPaginas.value !== ""
+  ) {
+    const novoLivro = new Livro(
+      inputTitulo.value,
+      inputAutor.value,
+      inputPaginas.value,
+      inputLido.checked
+    );
+    minhaBiblioteca.push(novoLivro);
+  }
+}
