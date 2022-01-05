d04091eb0ad9ebbefb3c2b040d48ac4735d51833
Adriel <adriel.fariasan@gmail.com>
2022-01-04 23:43:50 -0300

    Implementa livroParaHtml parcialmente

--- script.js
+++ script.js
@@ -56,3 +56,59 @@ function adicionarNaBiblioteca() {
     minhaBiblioteca.push(novoLivro);
   }
 }
+
+// gera elemento html para livro
+function livroParaHtml(livro) {
+  // card de livro
+  const html = document.createElement("div");
+  html.classList.add("livro");
+  if (livro.lido) {
+    html.classList.add("lido");
+  }
+
+  // titulo do card
+  const titulo = document.createElement("h3");
+  titulo.innerText = livro.titulo;
+  html.appendChild(titulo);
+
+  // autor do card
+  const autorP = document.createElement("p");
+  const autorIcone = document.createElement("i");
+  const autorSpan = document.createElement("span");
+
+  autorIcone.classList.add("fas", "fa-user");
+  autorP.appendChild(autorIcone);
+
+  autorSpan.innerText = livro.autor;
+  autorP.appendChild(autorSpan);
+
+  html.appendChild(autorP);
+
+  // publicacao
+  const pubP = document.createElement("p");
+  const pubIcone = document.createElement("i");
+  const pubSpan = document.createElement("span");
+
+  pubIcone.classList.add("fas", "fa-calendar-alt");
+  pubP.appendChild(pubIcone);
+
+  pubSpan.innerText = livro.publicacao;
+  pubP.appendChild(pubSpan);
+
+  html.appendChild(pubP);
+
+  // numero de paginas
+  const paginasP = document.createElement("p");
+  const paginasIcone = document.createElement("i");
+  const paginasSpan = document.createElement("span");
+
+  paginasIcone.classList.add("fas", "fa-book-open");
+  paginasP.appendChild(paginasIcone);
+
+  paginasSpan.innerText = `${livro.paginas} páginas`;
+  paginasP.appendChild(paginasSpan);
+
+  html.appendChild(paginasP);
+
+  return html;
+}
