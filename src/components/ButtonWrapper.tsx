ff0503552b75b06d141ecb7d74e1e6dc21d01973
Adriel <adriel.fsantos@outlook.com>
2022-12-21 17:05:26 -0300

    Corrige css de ButtonWrapper #1

--- src/components/ButtonWrapper.tsx
+++ src/components/ButtonWrapper.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
 const ButtonWrapper = styled.section`
+  background-color: #2b2d42;
+  color: #fff;
   padding: 0.5rem 1rem;
   display: flex;
   justify-content: center;
   gap: 1rem;
-  background-color: #2b2d42;
-  color: #fff;
 
-  & button {
+  button {
     background-color: #edf2f4;
   }
 
-  & button:hover {
+  button:hover {
     background-color: #feffff;
   }
 
