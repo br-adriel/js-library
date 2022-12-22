bf4739ead6778fb1a5e17664fbd2e7cfdf53c36b
Adriel <adriel.fsantos@outlook.com>
2022-12-22 15:22:27 -0300

    Insere BookContext e BookGrid em App #1

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,32 @@ import GlobalStyles from './components/GlobalStyles';
 import Header from './components/Header';
 import ModalAddBook from './components/ModalAddBook';
 import ModalContext from './contexts/ModalContext';
+import { Livro } from './global/types';
+import BooksContext from './contexts/BooksContext';
+import BookGrid from './components/BookGrid';
 
 function App() {
   const [modalState, setModalState] = useState<{ show: boolean }>({
     show: false,
   });
+
+  const [booksState, setBooksState] = useState<{ books: Livro[] }>({
+    books: [],
+  });
+
   return (
     <>
-      <ModalContext.Provider value={{ modalState, setModalState }}>
-        <GlobalStyles />
-        <Header />
-        <main></main>
-        <Footer />
-        <ModalAddBook />
-      </ModalContext.Provider>
+      <BooksContext.Provider value={{ booksState, setBooksState }}>
+        <ModalContext.Provider value={{ modalState, setModalState }}>
+          <GlobalStyles />
+          <Header />
+          <main>
+            <BookGrid />
+          </main>
+          <Footer />
+          <ModalAddBook />
+        </ModalContext.Provider>
+      </BooksContext.Provider>
     </>
   );
 }
