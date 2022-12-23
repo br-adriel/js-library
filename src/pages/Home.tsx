6b5cf3267999e48359507b7961a97da1dfccd300
Adriel <adriel.fsantos@outlook.com>
2022-12-23 13:37:09 -0300

    Adiciona pagina Home #2

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import BookGrid from '../components/BookGrid';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <BookGrid />
+      </main>
+    </>
+  );
+};
+
+export default Home;
